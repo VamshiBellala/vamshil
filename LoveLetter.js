ba8581b6497c2fa47a25ea314578df1e1b08ccfe
VamshiBellala <118789171+VamshiBellala@users.noreply.github.com>
2024-07-26 17:31:52 +0530

    Add files via upload

---
+++ LoveLetter.js
@@ -0,0 +1,47 @@
+import React, { useState, useRef } from 'react';
+import './LoveLetter.css';
+import audioFile from './kushi.mp3';
+
+const LoveLetter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullSize, setIsFullSize] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleOpenLetter = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsFullSize(true);
+      // Ensuring audio play is directly a result of this user interaction
+      if (audioRef.current) {
+        audioRef.current.play()
+          .then(() => console.log("Playback succeeded"))
+          .catch(e => console.error("Playback failed:", e));
+      }
+    }, 800);
+  };
+
+  const handleCloseLetter = () => {
+    setIsFullSize(false);
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+      setIsOpen(false);
+    }, 800);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
+        Dear Vasu, Happy Birthday!🖤,<br />
+        From the moment we met, you've brought endless joy into my life. I’m not sure how much I mean to you, but you hold a special place in my heart...<br />Here’s to celebrating you today and always, and to many more moments together in the future.)<br />
+        Always yours, @Vamshi Bellala
+      </div>
+      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
+  );
+};
+
+export default LoveLetter;
